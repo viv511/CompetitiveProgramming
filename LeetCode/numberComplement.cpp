fbcc0f21345f4780bb0962d03092ed44ab63f6a0
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2024-08-22 18:46:04 -0400

    LC: NumberComplement
    Learned bit-masking and bit-wise operator techniques

--- LeetCode/numberComplement.cpp
+++ LeetCode/numberComplement.cpp
@@ -2,12 +2,35 @@
 
 using namespace std;
 
+void printBits(int n) {
+	for(int k=31; k>=0; k--) {
+		if(n&(1<<k)) {
+			cout << "1";
+		}
+		else {
+			cout << "0";
+		}
+	}
+
+	cout << "\n";
+}
 
 //Bit masks!
 class Solution {
 public:
-    int findComplement(int num) {
-        // cout << __builtin_clz(num);
-        return ~num;
+    int findComplement(int n) {
+        //5 == 101 ==> flip is 010 == 2
+        // printBits(n);
+
+        int maskLength = 32-__builtin_clz(n);
+
+        for(int i=0; i<maskLength; i++) {
+            //1 << i gives us a "1" in the i-th bit
+            //XOR each time to get the new number
+            n = n ^ (1 << i);
+            // printBits(n);
+        }
+
+        return n;
     }
 };
