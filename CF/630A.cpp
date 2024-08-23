1a8f3cfce55587edc02622fb448e6e68c09a88fd
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2024-08-22 22:51:36 -0400

    CF: 630A

---
+++ CF/630A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+ 
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define all(v) v.begin(), v.end()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define ins insert
+#define sq(x) ((x) * (x))
+#define sz(x) (int)(x.size())
+
+using namespace std;
+
+void solve() {
+	cout << 25 << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T = 1;
+	while(T--) {
+		solve();
+	}
+}
