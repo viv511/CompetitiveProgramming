743b79a8fc0a3c1d9dccd6563b00e893279bddb0
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2024-08-31 21:42:58 -0400

    CF: 2010A

---
+++ CF/2010A.cpp
@@ -0,0 +1,57 @@
+/*
+4 3
+2 1 1
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+ 
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define all(v) v.begin(), v.end()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define ins insert
+#define sq(x) ((x) * (x))
+#define sz(x) (int)(x.size())
+#define print(answer) cout << answer << "\n"
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    int ans = 0;
+    for(int i=0; i<n; i++) {
+        int a; cin >> a;
+        if(i%2 == 0) {
+            ans += a;
+        }
+        else {
+            ans -= a;
+        }
+    }
+
+    cout << ans << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T; cin >> T;
+	while(T--) {
+		solve();
+	}
+}
