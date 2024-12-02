66392d33211d9a0330dd8d47e1c9d00ebb380fc7
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2024-12-02 00:03:27 -0500

    AoC Day 1

---
+++ AdventOfCode2024/1b_2024.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+ 
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define all(v) v.begin(), v.end()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define ins insert
+#define sz(x) (int)(x.size())
+#define yap(answer) cout << answer << "\n"
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n = 1000;
+	vector<int> a(n);
+	vector<int> b(n);
+
+    map<int, int> bCount;
+
+    long long ans = 0;
+	for(int i=0; i<n; i++) {
+		cin >> a[i] >> b[i];
+        bCount[b[i]]++;
+	}
+
+    sort(all(a));
+    sort(all(b));
+
+    for(int i=0; i<n; i++) {
+		ans += a[i]*bCount[a[i]];
+	}
+
+    yap(ans);
+}
