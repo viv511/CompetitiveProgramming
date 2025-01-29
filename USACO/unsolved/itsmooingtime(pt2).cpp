9b16ea0d3206d9037ce19a907f8a5f5da950fb93
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2025-01-29 00:13:31 -0500

    USACO #2 Jan25
    Was clowning, missed key insight
    
    Should work

--- USACO/unsolved/itsmooingtime(pt2).cpp
+++ USACO/unsolved/itsmooingtime(pt2).cpp
@@ -10,29 +10,31 @@ typedef vector<int> vi;
 
 int main() {
     int n; cin >> n;
-    vi v(n);
 
+    vi v(n);
+    map<int, int> freq;
+    vi distinct(n);
+
+    set<int> s;
     for(int i=0; i<n; i++) {
         cin >> v[i];
+        s.insert(v[i]);
+        distinct[i] = s.size();
+        freq[v[i]]++;
     }  
 
-    map<int, int> m;
     ll ans = 0;
-    set<int> s;
-
-    for(int i=n-1; i>=0; i--) {
+    
+    map<int, int> m;
+    for(int i=n-1; i>0; i--) {
         int num = v[i];
         m[num]++;
 
         if(m[num] == 2) { //two occurances --> search for distinct nums
-            for(int j=0; j<i; j++) {
-                if(v[j] != num) {
-                    s.insert(v[j]);
-                }
+            ans += distinct[i-1];
+            if(freq[num] > 2) {
+                ans--;
             }
-
-            ans += s.size();
-            s.clear();
         }  
     }
 
