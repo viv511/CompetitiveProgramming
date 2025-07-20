0c274796d7dfeb15e6300c57dc9fe8b37a1655cc
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2025-07-20 11:49:30 -0400

    MACSO Summer B

--- MACSO/Summer/B.cpp
+++ MACSO/Summer/B.cpp
@@ -1,47 +1,71 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 #include <cmath>
-#include <map>
-#include <set>
-#include <queue>
 
 using namespace std;
-
 typedef long long ll;
-typedef unsigned long long ull; 
-typedef vector<int> vi;
-typedef pair<int, int> pi;
 
-#define all(v) v.begin(), v.end()
-#define mp make_pair
-#define fi first
-#define se second
-#define pb push_back
-#define ins insert
-#define sz(x) (int)(x.size())
 #define yap(answer) cout << answer << "\n"
-#define FOR(i, a, b) for (int i=a; i<(b); i++)
-#define F0R(i, a) FOR(i, 0, a)
+
 
 void solve() {
-	ll k, x; cin >> k >> x;
-
-    ll ans = 0;
-    ll days = 0;
-    for(int i=1; i<=k; i++) {
-        if(days >= x) break;
-        days += i;
-        ans++;
-    }
-    for(int i=k-1; i>=1; i--) {
-        if(days >= x) break;
-        days += i;
-        ans++;
-    }
+    ll k, x; 
+    cin >> k >> x;
     
-    yap(ans);
+    ll halfway = (k + 1) * k / 2ll;
+
+    if(x >= (k * k)) {
+        yap(2 * k - 1);
+    }
+    else if(x > halfway) {
+        ll ans = k;
+        x -= halfway;
+
+        //k-1, k-2, k-3 ... 1
+        //add one to ans and subtract k-1, then k-2, etc
+        
+        // ll i = 1;
+        // while(x > 0 && (k-i) > 0) {
+        //     ans++;
+        //     if(x >= (k - i)) {
+        //         x -= (k - i);
+        //         i++;
+        //     }
+        //     else {
+        //         break;
+        //     }
+        // }
+
+        //sum of k-1, k-2, k-3 ... 1
+        //s = n(a1 + an)/2
+
+        ll L = 1;
+        ll R = k - 1;
+        
+        //run bin search
+        while (L <= R) {
+            ll M = (L + R) / 2;
+            ll i = M;
+
+            ll S = ((2*k - i - 1) * i)/ 2;
+
+            if (S >= x) {
+                R = M - 1;
+
+                ans = k + i;
+            } else {
+                L = M + 1;
+            }
+        }
+
+        yap(ans);
+    }
+    else {
+        //expanded version of quadratic formula
+        //n(n+1)/2 >= x
+        // n^2 + n - 2x >= 0
+        // n = (-1 + sqrt(1 + 8x)) / 2
+        yap(ceil((-1ll + sqrt(1 + 8ll * x)) / 2ll));
+    }
 }
 
 int main() {
