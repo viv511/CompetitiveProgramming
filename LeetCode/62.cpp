98611b92806299e4497b3b01a744904716338eea
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2025-12-05 01:19:00 -0500

    LC: 62

---
+++ LeetCode/62.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+long long choose(int a, int b) {
+    if (b > a - b) {
+        b = a - b;
+    }
+
+    long long res = 1;
+    for (int i = 1; i <= b; i++) {
+        res = res * (a - b + i) / i;
+    }
+
+    return res;
+}
+
+int uniquePaths(int m, int n) {
+    m--;
+    n--;
+
+    if(m == 0 || n == 0) return 1;
+
+    //m + n choose m
+    return choose(m + n, m);
+}
