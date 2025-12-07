3cd25e6c18b85b3d1911c0664ba8d20bd48b22c3
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2025-12-07 16:55:24 -0500

    LC 104

---
+++ LeetCode/104.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode* root) {
+    if(root == nullptr) return 0;
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}   
