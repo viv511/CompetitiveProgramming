2c939c2d6672842098403b6f27d4c5d7c06ba33b
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2026-05-30 01:27:15 -0400

    LC 1791 - i.... am pretty silly O(n)->O(1)

--- LeetCode/1791.cpp
+++ LeetCode/1791.cpp
@@ -2,21 +2,8 @@
 using namespace std;
 
 int findCenter(vector<vector<int>>& edges) {
-    int n = edges.size() + 1;
-    vector<int> count(n, 0);
-
-    for(int i = 0; i < edges.size(); i++) {
-        count[edges[i][0] - 1]++;
-        count[edges[i][1] - 1]++;
-    }       
-
-    for(int i = 0; i < n; i++) {
-        if(count[i] == n-1) {
-            return i+1;
-        }
-    }
-
-
-    // assert(impossible)
-    return -1;
+    int a = edges[0][0];
+    int b = edges[0][1];
+    if(a == edges[1][0] || a == edges[1][1]) return a;
+    else return b;
 }
