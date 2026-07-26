892ee3803558d7f4a8d0ce27c59ae60b21293407
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2026-07-26 17:28:53 -0400

    CF 1873C

---
+++ CF/solved/1873C.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+int board[10][10] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+    {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+void solve() {
+    int ans = 0;
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            char c; cin >> c;
+
+            if (c == 'X') {
+                ans += board[i][j];
+            }
+        }
+    }
+
+    cout << ans << "\n";
+}   
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T; cin >> T;
+	while(T--) {
+		solve();
+	}
+}
