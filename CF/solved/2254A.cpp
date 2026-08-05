292f5af5e6e05b94f0ba73c8ce4a3b05e4925c38
Vivek Mehta <66559749+viv511@users.noreply.github.com>
2026-08-04 22:38:53 -0400

    CF 2254A

---
+++ CF/solved/2254A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    vector<int> v(3);
+	cin >> v[0] >> v[1] >> v[2];
+    
+    sort(v.begin(), v.end());
+
+    cout << min(v[2]-v[1], v[1]-v[0]) << "\n";
+}  
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T; cin >> T;
+	while(T--) {
+		solve();
+	}
+}
